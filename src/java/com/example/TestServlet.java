2a45b324a1ddac7bddc26159ddd772ff339b74b9
Flex <Flex@DESKTOP-JMDKINH>
2020-06-21 09:13:03 +0800

    Change Test Servlet

--- src/java/com/example/TestServlet.java
+++ src/java/com/example/TestServlet.java
@@ -32,13 +32,15 @@ public class TestServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        //Continue some editing
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet TestServlet</title>");            
+            out.println("<title>Change some editing</title>");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet TestServlet at " + request.getContextPath() + "</h1>");
